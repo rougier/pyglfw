9f9db3d2ebd0415fcf0a900a27c9ca320a766a4f
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2013-06-25 07:27:29 +0200

    Added rotating triangle from GLFW documentation

--- simple.py
+++ simple.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     import OpenGL.GL as gl
 
     def on_key(window, key, scancode, action, mods):
-        if key == glfw.KEY_ESCAPE:
+        if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
             glfw.SetWindowShouldClose(window,1)
 
     # Initialize the library
@@ -63,7 +63,24 @@ if __name__ == '__main__':
     # Loop until the user closes the window
     while not glfw.WindowShouldClose(window):
         # Render here
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        width, height = glfw.GetFramebufferSize(window)
+        ratio = width / float(height)
+        gl.glViewport(0, 0, width, height)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+        gl.glOrtho(-ratio, ratio, -1, 1, 1, -1)
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+        gl.glLoadIdentity()
+        gl.glRotatef(glfw.GetTime() * 50, 0, 0, 1)
+        gl.glBegin(gl.GL_TRIANGLES)
+        gl.glColor3f(1, 0, 0)
+        gl.glVertex3f(-0.6, -0.4, 0)
+        gl.glColor3f(0, 1, 0)
+        gl.glVertex3f(0.6, -0.4, 0)
+        gl.glColor3f(0, 0, 1)
+        gl.glVertex3f(0, 0.6, 0)
+        gl.glEnd()
 
         # Swap front and back buffers
         glfw.SwapBuffers(window)
